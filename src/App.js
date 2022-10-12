4862ca1f1ff78db97e187306098da0e741a47b8f
devloper solaiman <you@example.com>
2022-10-12 07:42:17 +0600

    some update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Component/Home/Home';
+import Home from "./Pages/Home/Home";
+
 
 function App() {
   return (
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <small>header</small>
+        </header>
+    );
+};
+
+export default Header;
